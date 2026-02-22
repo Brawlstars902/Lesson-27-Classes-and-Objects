b9c32379ce1f35342689c4d4cb501a22e64a0f27
Brawlstars902 <aparna.rautela@gmail.com>
2026-02-22 07:09:42 +0530

    Add files via upload

---
+++ Car.py
@@ -0,0 +1,11 @@
+class vehichle:
+
+    def __init__(self,max_speed,mileage):
+
+        self.max_speed=max_speed
+        self.mileage=mileage
+
+Modelx=vehichle(240,18)
+
+print('Model Max Speed:  ',Modelx.max_speed)
+print('Model Mileage:  ',Modelx.mileage)
